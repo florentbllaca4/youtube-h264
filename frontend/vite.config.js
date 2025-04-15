5d381d5021491598174d8c21e664e6531fc3de0b
florentbllaca4 <bllacaflorent31@gmail.com>
2025-04-15 21:01:03 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/download': {
+        target: 'https://backend-youtubeh264.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/file': {
+        target: 'https://backend-youtubeh264.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 })
