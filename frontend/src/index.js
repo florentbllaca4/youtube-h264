b6a03e5232260faf84d4f4a3e91db6048b5c042f
florentbllaca4 <bllacaflorent31@gmail.com>
2025-04-15 21:37:16 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';  // Shtoni CSS-in tuaj, siç kërkohet për Tailwind, etj.
+import './style.css';  // Shtoni CSS-in tuaj, siç kërkohet për Tailwind, etj.
 import App from './App';  // Ose cilado komponent që keni për aplikacionin tuaj
 import reportWebVitals from './reportWebVitals';
 
