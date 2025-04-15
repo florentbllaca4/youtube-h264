58c7ee715d78a55496b1626cd2c2e126f75bf435
florentbllaca4 <bllacaflorent31@gmail.com>
2025-04-15 20:57:33 +0200

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
