66938d5285ff4f5253c94dc6c8c614a4649455c9
florentbllaca4 <bllacaflorent31@gmail.com>
2025-04-15 20:57:15 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,  // Hap automatikisht shfletuesin
-    port: 5173   // Sigurohuni që porti është i saktë
+    port: 5173
   }
-});
+})
